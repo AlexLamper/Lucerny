681abbd2fab5bb48510fad8c1e85070439f146a8
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2023-11-23 08:13:59 +0100

    Added Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,9 @@ const Home = () => {
       </Head>
 
       {/* Hero Section */}
-      <section className="flex flex-col-reverse lg:flex-row items-center justify-center lg:justify-between h-screen py-16 lg:py-0">
+      <section className="container mx-auto flex flex-col lg:flex-row items-center justify-center h-screen py-16 lg:py-0">
         {/* Content Section */}
-        <div className="max-w-4xl text-center lg:text-left lg:w-1/2 px-8 lg:px-16">
+        <div className="max-w-[80%] w-4/5 lg:w-1/2 mx-auto lg:mx-0 lg:order-1 lg:text-left lg:py-12">
           <h1 className="text-4xl md:text-6xl font-bold mb-4">Welkom bij Lucerny</h1>
           <p className="text-lg md:text-xl mb-8">Uw partner in op maat gemaakte websites</p>
           <a
@@ -29,11 +29,11 @@ const Home = () => {
         </div>
 
         {/* Image Section */}
-        <div className="lg:w-1/2">
+        <div className="max-w-[80%] w-4/5 lg:w-1/2 lg:order-2 lg:py-12">
           <Image
-            src="/images/main02.svg"
+            src="/images/main02.svg" // Replace with your image path
             alt="Hero Image"
-            className="lg:rounded-lg"
+            className="w-full h-auto lg:rounded-lg"
             width={400}
             height={400}
           />
@@ -44,7 +44,6 @@ const Home = () => {
       {/* Information about us */}
       {/* Call to action */}
       {/* Footer */}
-
     </div>
   );
 };
