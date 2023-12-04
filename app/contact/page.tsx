7df39c1bf5411167aee528be2f597f726a95acf9
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2023-12-04 12:54:21 +0100

    Added Working Contact Form Using FormCarry

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,42 +2,11 @@
 import Head from 'next/head';
 import type { Metadata } from 'next';
 import ContactForm from '@/components/Contact/ContactForm';
-import ClientSideComponent from '@/components/TestComponents/ClientSideComponent';
-import BasicForm from '@/components/BasicForm';
-// import { useForm } from '@formcarry/react';
 
 export const metadata: Metadata = {
   title: 'Contact | Lucerny',
   description: 'Lucerny is a web development agency that specializes in building websites and web applications for small businesses and startups.',
 };
-
-// function MyFormcarry() {
-//   const {state: formcarryState, submit: formcarrySubmit} = useForm({
-// 	id: 'Your-Form-ID-From-Formcarry'
-//   });
-
-//   if (formcarryState.submitted) {
-//     return <div>Thank you! We received your submission.</div>;
-//   }
-
-//   return (
-//     <form onSubmit={formcarrySubmit}>
-// 		<label htmlFor="name">Name</label>
-// 		<input id="name" type="text" name="text" />
-
-// 		<label htmlFor="surname">Surname</label>
-// 		<input id="surname" type="text" name="surname" />
-		
-// 		<label htmlFor="email">Email</label>
-// 		<input id="email" type="email" name="email" />
-		
-// 		<label htmlFor="message">Message</label>
-// 		<textarea id="message" name="message" />
-		
-// 		<button type="submit">Send</button>
-//     </form>
-//   );
-// }
 
 export default function Contact() {
   return (
@@ -55,9 +24,7 @@ export default function Contact() {
         <p className="text-lg text-gray-600 max-w-[50%] mx-auto">Vragen of iets onduidelijk? Vul het formulier hieronder in en we nemen zo snel mogelijk contact met je op.</p>
       </div>
 
-      {/* <ContactForm /> */}
-      {/* <ContactForm /> */}
-      <BasicForm />
+      <ContactForm />
     </div>
   );
 }
--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -1,85 +1,69 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react';
 import styles from '@/styles/contact.module.css';
 
-const ContactForm = () => {
+export default function BasicForm() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const [error, setError] = useState('')
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+    e.stopPropagation();
+
+    fetch("https://formcarry.com/s/rUsMK5VhPc", {
+      method: 'POST',
+      headers: { 
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name, email, message })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response.code === 200) {
+        alert("We received your submission, thank you!");
+      }
+      else if(response.code === 422){
+        // Field validation failed
+        setError(response.message)
+      }
+      else {
+        // other error from formcarry
+        setError(response.message)
+      }
+    })
+    .catch(error => {
+      // request related error.
+      setError(error.message ? error.message : error);
+    });
+  }
+
+
   return (
-    <div>
-        <form className="max-w-2xl mx-auto p-8">
-        <div className="grid grid-cols-2 gap-6">
-          <div>
-            <label htmlFor="firstName" className="block text-base font-bold text-gray-700">
-              Naam
-            </label>
-            <input
-              className={`${styles.input} w-30 h-12`}
-              type="text"
-              id="Name"
-              name="Name"
-              placeholder="Volledige naam"
-              style={{ paddingLeft: '0.75rem' }}
-            />
-          </div>
-          <div>
-            <label htmlFor="lastName" className="block text-base font-bold text-gray-700">
-              Bedrijf
-            </label>
-            <input
-              type="text"
-              id="bedrijf"
-              name="bedrijf"
-              className={`${styles.input} w-30 h-12`}
-              placeholder="Bedrijfsnaam"
-              style={{ paddingLeft: '0.75rem' }}
-            />
-          </div>
-          <div>
-            <label htmlFor="email" className="block text-base font-bold text-gray-700">
-              E-mailadres
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className={`${styles.input} w-30 h-12`}
-              placeholder="E-mailadres"
-              style={{ paddingLeft: '0.75rem' }}
-            />
-          </div>
-          <div>
-            <label htmlFor="phoneNumber" className="block text-base font-bold text-gray-700">
-              Telefoonnummer
-            </label>
-            <input
-              type="tel"
-              id="phoneNumber"
-              name="phoneNumber"
-              className={`${styles.input} w-30 h-12`}
-              placeholder="Telefoonnummer"
-              style={{ paddingLeft: '0.75rem' }}
-            />
-          </div>
-          <div className="col-span-2">
-            <label htmlFor="message" className="block text-base font-bold text-gray-700">
-              Wat kunnen we voor je doen?
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              rows={6} // Increased the rows to make it larger
-              className={`${styles.input} w-full`}
-              placeholder="Licht je wensen zo gedetailleerd toe."
-              style={{ paddingLeft: '0.75rem', paddingTop: '0.75rem' }}
-            />
-          </div>
-        </div>
-        <div className="mt-8 text-center">
-          <button type='submit' className={styles.button}>
-            <span>Verstuur</span>
-          </button>
-        </div>
-      </form>
-    </div>
+    <form onSubmit={(e) => onSubmit(e)} className={styles.form}>
+    
+      <div className="formcarry-block">
+        <label htmlFor="name" className="block text-base font-bold text-gray-700">Volledige Naam</label>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} id="name" placeholder="Uw voor en achternaam" className={styles.input} />
+      </div>
+      
+      <div className="formcarry-block">
+        <label htmlFor="email" className="block text-base font-bold text-gray-700">E-mailadres</label>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" placeholder="voorbeeld@gmail.com" className={styles.input} />
+      </div>
+      
+      <div className="formcarry-block">
+        <label htmlFor="message" className="block text-base font-bold text-gray-700">Wat kunnen we voor je doen?</label>
+        <textarea value={message} onChange={(e) => setMessage(e.target.value)} id="message" placeholder="Licht je wensen zo gedetailleerd toe." className={styles.input}></textarea>
+      </div>
+      
+      <div className="formcarry-block">  
+        <button type="submit" className={styles.button}>Send</button>
+      </div>
+    </form>
   )
 }
-
-export default ContactForm
