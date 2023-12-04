6fdc87f72d1018c240b0f541aa704a32e93183fa
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2023-12-04 12:35:59 +0100

    Added Contact Form Using FormCarry

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,11 +3,41 @@ import Head from 'next/head';
 import type { Metadata } from 'next';
 import ContactForm from '@/components/Contact/ContactForm';
 import ClientSideComponent from '@/components/TestComponents/ClientSideComponent';
+import BasicForm from '@/components/BasicForm';
+// import { useForm } from '@formcarry/react';
 
 export const metadata: Metadata = {
   title: 'Contact | Lucerny',
   description: 'Lucerny is a web development agency that specializes in building websites and web applications for small businesses and startups.',
 };
+
+// function MyFormcarry() {
+//   const {state: formcarryState, submit: formcarrySubmit} = useForm({
+// 	id: 'Your-Form-ID-From-Formcarry'
+//   });
+
+//   if (formcarryState.submitted) {
+//     return <div>Thank you! We received your submission.</div>;
+//   }
+
+//   return (
+//     <form onSubmit={formcarrySubmit}>
+// 		<label htmlFor="name">Name</label>
+// 		<input id="name" type="text" name="text" />
+
+// 		<label htmlFor="surname">Surname</label>
+// 		<input id="surname" type="text" name="surname" />
+		
+// 		<label htmlFor="email">Email</label>
+// 		<input id="email" type="email" name="email" />
+		
+// 		<label htmlFor="message">Message</label>
+// 		<textarea id="message" name="message" />
+		
+// 		<button type="submit">Send</button>
+//     </form>
+//   );
+// }
 
 export default function Contact() {
   return (
@@ -26,7 +56,8 @@ export default function Contact() {
       </div>
 
       {/* <ContactForm /> */}
-      <ContactForm />
+      {/* <ContactForm /> */}
+      <BasicForm />
     </div>
   );
 }
