cf272d762ac6deb97a4324ea94bef7ce02b64a84
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2023-12-05 23:11:18 +0100

    Made Ervaringen Page Responsive

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -83,7 +83,7 @@ export default function Portfolio() {
         <p className="text-lg text-gray-600">Ontdek ons portfolio van projecten</p>
       </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-6">
             {projects.map((project) => (
                 <div key={project.id} className="bg-white rounded-lg shadow-md p-6">
                     <Link href={project.link || '#'}>
