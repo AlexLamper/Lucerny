a5c8d3ca51ebe4e3397ce377d02ea9758ade1185
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2023-12-11 10:07:46 +0100

    Changed Favicon (trying to make it load)

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,10 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
     </>
   );
 }
