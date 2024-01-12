0cdb625a3bc8b86042d0dd087d93bb2155a15f32
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2024-01-12 15:24:08 +0100

    Added Contact Form

--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -1,69 +1,97 @@
-"use client";
+'use client';
 
-import React, { useState } from 'react';
-import styles from '@/styles/contact.module.css';
+import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { sendEmail } from '@/utils/send-email';
+import styles from '@/styles/contact.module.css'
 
-export default function BasicForm() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [message, setMessage] = useState('')
+export type FormData = {
+  firstName: string;
+  surName: string;
+  phone: string;
+  email: string;
+  company: string;
+  message: string;
+};
 
-  const [error, setError] = useState('')
+const ContactForm: FC = () => {
+  const { register, handleSubmit, setValue } = useForm<FormData>();
 
-  function onSubmit(e: React.FormEvent<HTMLFormElement>){
-    e.preventDefault();
-    e.stopPropagation();
+  function onSubmit(data: FormData) {
+    sendEmail(data);
 
-    fetch("https://formcarry.com/s/Vb1mWIo2Xm", {
-      method: 'POST',
-      headers: { 
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ name, email, message })
-    })
-    .then(response => response.json())
-    .then(response => {
-      if (response.code === 200) {
-        alert("We hebben je bericht ontvangen, bedankt!");
-      }
-      else if(response.code === 422){
-        // Field validation failed
-        setError(response.message)
-      }
-      else {
-        // other error from formcarry
-        setError(response.message)
-      }
-    })
-    .catch(error => {
-      // request related error.
-      setError(error.message ? error.message : error);
-    });
+    setValue('firstName', '');
+    setValue('surName', '');
+    setValue('phone', '');
+    setValue('email', '');
+    setValue('company', '');
+    setValue('message', '');
   }
 
-
   return (
-    <form onSubmit={(e) => onSubmit(e)} className={styles.form}>
-    
-      <div className="formcarry-block">
-        <label htmlFor="name" className="block text-base font-bold text-gray-700">Volledige Naam</label>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} id="name" placeholder="Uw voor en achternaam" className={styles.input} />
-      </div>
-      
-      <div className="formcarry-block">
-        <label htmlFor="email" className="block text-base font-bold text-gray-700">E-mailadres</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" placeholder="voorbeeld@gmail.com" className={styles.input} />
-      </div>
-      
-      <div className="formcarry-block">
-        <label htmlFor="message" className="block text-base font-bold text-gray-700">Wat kunnen we voor je doen?</label>
-        <textarea value={message} onChange={(e) => setMessage(e.target.value)} id="message" placeholder="Licht je wensen zo gedetailleerd toe. Is het voor uw bedrijf of voor u zelf etc..." className={styles.inputTextArea}></textarea>
-      </div>
-      
-      <div className="formcarry-block">  
+    <form onSubmit={handleSubmit(onSubmit)} className='bg-[#e7e9f1] p-6 rounded-lg border border-[#cdced3]'>
+
+        <label htmlFor="name" className="block text-base font-bold text-gray-700 mb-2">Volledige Naam</label>
+        <div className="flex mb-2">
+          <input
+            type="text"
+            placeholder="Voornaam"
+            className="w-1/2 mr-2 rounded-md py-3 px-4 bg-white text-black focus:outline-none focus:bg-gray-200"
+            {...register('firstName', { required: true })}
+          />
+          <input
+            type="text"
+            placeholder="Achternaam"
+            className="w-1/2 ml-2 rounded-md py-3 px-4 bg-white text-black focus:outline-none focus:bg-gray-200"
+            {...register('surName', { required: true })}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-base font-bold text-gray-700 mb-2">Telefoon</label>
+          <input
+            type="tel"
+            placeholder="+31 6 12345678"
+            className="w-full rounded-md py-3 px-4 bg-white text-black focus:outline-none focus:bg-gray-200"
+            {...register('phone', { required: true })}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-base font-bold text-gray-700 mb-2">E-mailadres</label>
+          <input
+            type="email"
+            placeholder="voorbeeld@gmail.com"
+            className="w-full rounded-md py-3 px-4 bg-white text-black focus:outline-none focus:bg-gray-200"
+            {...register('email', { required: true })}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-base font-bold text-gray-700 mb-2">Bedrijfsnaam*</label>
+          <input
+            type="text"
+            placeholder="Uw bedrijfsnaam (optioneel)"
+            className="w-full rounded-md py-3 px-4 bg-white text-black focus:outline-none focus:bg-gray-200"
+            {...register('company', { required: true })}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-base font-bold text-gray-700 mb-2">Wat kunnen we voor je doen?</label>
+          <textarea
+            placeholder="Licht uw wensen zo gedetailleerd toe. Is het voor uw bedrijf, voor u zelf etc..."
+            rows={5}
+            cols={40}
+            maxLength={200}
+            className="w-full rounded-md py-3 px-4 bg-white text-black focus:outline-none focus:bg-gray-200 resize-none"
+            {...register('message', { required: true })}
+          />
+        </div>
+
         <button type="submit" className={styles.button}>Verzend uw bericht</button>
-      </div>
     </form>
   )
 }
+
+export default ContactForm;
