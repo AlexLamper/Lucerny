e5a0e75cc7200c9d7a7020226778365b64474e40
Alex Lamper <lamp0020@hz.nl>
2024-04-19 17:43:32 +0100

    Updated Contact Page Design

--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -35,67 +35,67 @@ const ContactForm: FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='bg-[#141414e1] p-6 border border-[#1b1b1bb0] rounded-lg'>
-      <label htmlFor="name" className="block text-base font-medium text-white tracking-wide mb-2">Volledige Naam</label>
+    <form onSubmit={handleSubmit(onSubmit)} className='p-6 border border-[#1b1b1b50] rounded-3xl'>
+      <label htmlFor="name" className="block text-base font-medium text-black tracking-wide mb-2">Volledige Naam</label>
         <div className="flex mb-2">
           <input
             type="text"
             placeholder="Voornaam"
-            className="w-1/2 mr-2 py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1bb0]"
+            className="w-1/2 mr-2 py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1b50] rounded-lg"
             {...register('firstName', { required: true })}
           />
           <input
             type="text"
             placeholder="Achternaam"
-            className="w-1/2 ml-2 py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1bb0]"
+            className="w-1/2 ml-2 py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1b50] rounded-lg"
             {...register('surName', { required: true })}
           />
         </div>
 
         <div className="mb-4">
-          <label htmlFor="name" className="block text-base font-medium text-white tracking-wide mb-2">Telefoon</label>
+          <label htmlFor="name" className="block text-base font-medium text-black tracking-wide mb-2">Telefoon</label>
           <input
             type="tel"
             placeholder="+31 6 12345678"
-            className="w-full py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1bb0]"
+            className="w-full py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1b50] rounded-lg"
             {...register('phone', { required: true })}
           />
         </div>
 
         <div className="mb-4">
-          <label htmlFor="name" className="block text-base font-medium text-white tracking-wide mb-2">E-mailadres</label>
+          <label htmlFor="name" className="block text-base font-medium text-black tracking-wide mb-2">E-mailadres</label>
           <input
             type="email"
             placeholder="voorbeeld@gmail.com"
-            className="w-full py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1bb0]"
+            className="w-full py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1b50] rounded-lg"
             {...register('email', { required: true })}
           />
         </div>
 
         <div className="mb-4">
-          <label htmlFor="name" className="block text-base font-medium text-white tracking-wide mb-2">Bedrijfsnaam*</label>
+          <label htmlFor="name" className="block text-base font-medium text-black tracking-wide mb-2">Bedrijfsnaam*</label>
           <input
             type="text"
             placeholder="Uw bedrijfsnaam (optioneel)"
-            className="w-full py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1bb0]"
+            className="w-full py-3 px-4 bg-white text-black focus:outline-none border border-[#1b1b1b50] rounded-lg"
             {...register('company', { required: true })}
           />
         </div>
 
         <div className="mb-4">
-          <label htmlFor="name" className="block text-base font-medium text-white tracking-wide mb-2">Wat kunnen we voor je doen?</label>
+          <label htmlFor="name" className="block text-base font-medium text-black tracking-wide mb-2">Wat kunnen we voor je doen?</label>
           <textarea
             placeholder="Licht uw wensen zo gedetailleerd toe. Is het voor uw bedrijf, voor u zelf etc..."
             rows={5}
             cols={40}
             maxLength={200}
-            className="w-full py-3 px-4 bg-white text-black focus:outline-none resize-none border border-[#1b1b1bb0]"
+            className="w-full py-3 px-4 bg-white text-black focus:outline-none resize-none border border-[#1b1b1b50] rounded-lg"
             {...register('message', { required: true })}
           />
         </div>
         <button
-          className="bg-gradient-to-r from-blueButtonStart to-blueButtonEnd text-black py-3 px-8 rounded-xl text-lg transition duration-300 inline-block hover:bg-gradient-to-r hover:from-blueButtonHoverStart hover:to-blueButtonHoverEnd hover:shadow-lg"
-          style={{ background: "linear-gradient(90deg, #f5f8ff, #aac3f0)" }}
+          className="bg-gradient-to-r from-blueButtonStart to-blueButtonEnd text-white py-3 px-8 rounded-xl text-lg transition duration-300 inline-block hover:bg-gradient-to-r hover:from-blueButtonHoverStart hover:to-blueButtonHoverEnd hover:shadow-lg"
+          style={{ background: "linear-gradient(90deg, #7E6CED, #7E6CED)" }}
           type='submit'
         >
           Verzend uw bericht 
